b20293767cc26f497d95e152615db1fd4566daa9
Lyell Read <lyellread@gmail.com>
2018-03-30 22:39:54 -0700

    Cleanup (Minor)
    Cleaned up commented header on ADScene.swift to reflect new name of file, and relevant info, was duplicate before.

--- Space Arena/scenes/ADScene.swift
+++ Space Arena/scenes/ADScene.swift
@@ -6,14 +6,6 @@
 //  Copyright © 2018 EvryGame. All rights reserved.
 //
 
-//
-//  DeathScene.swift
-//  Space Arena
-//
-//  Created by Ever on 3/26/18.
-//  Copyright © 2018 EvryGame. All rights reserved.
-//
-
 
 import SpriteKit
 import GameplayKit
@@ -23,7 +15,7 @@ import GoogleMobileAds
 class AdScene: SKScene, GADRewardBasedVideoAdDelegate {
 	private var lastUpdateTime : TimeInterval = 0
 	var rewardBasedAd: GADRewardBasedVideoAd!
-	
+
 	func changeToMenuScene() {
 		let menu = MenuScene(fileNamed: "MenuScene")
 		menu?.scaleMode = .aspectFill
@@ -38,46 +30,46 @@ class AdScene: SKScene, GADRewardBasedVideoAdDelegate {
 			staminaCount += 1
 		}
 	}
-	
+
 	func rewardBasedVideoAdDidReceive(_ rewardBasedVideoAd:GADRewardBasedVideoAd) {
 		print("Reward based video ad is received.")
 		guard let controller = self.view?.window?.rootViewController as? GameViewController else {return}
 		rewardBasedAd.present(fromRootViewController: controller)
 	}
-	
+
 	func rewardBasedVideoAdDidOpen(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
 		print("Opened reward based video ad.")
 	}
-	
+
 	func rewardBasedVideoAdDidStartPlaying(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
 		print("Reward based video ad started playing.")
 	}
-	
+
 	func rewardBasedVideoAdDidCompletePlaying(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
 		print("Reward based video ad has completed.")
 		//changeToMenuScene()
 	}
-	
+
 	func rewardBasedVideoAdDidClose(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
 		print("Reward based video ad is closed.")
 		changeToMenuScene()
 	}
-	
+
 	func rewardBasedVideoAdWillLeaveApplication(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
 		print("Reward based video ad will leave application.")
 	}
-	
+
 	func rewardBasedVideoAd(_ rewardBasedVideoAd: GADRewardBasedVideoAd, didFailToLoadWithError error: Error) {
 		print("Reward based video ad failed to load.")
 		changeToMenuScene()
 	}
-	
-	
+
+
 	var entities = [GKEntity]()
 	var graphs = [String : GKGraph]()
-	
+
 	override func didMove(to view: SKView) {
-		
+
 		rewardBasedAd = GADRewardBasedVideoAd.sharedInstance()
 		rewardBasedAd.delegate = self
 		//rewardBasedAd.load(GADRequest(), withAdUnitID: "ca-app-pub-2732851918745448/1874443128")
@@ -87,6 +79,3 @@ class AdScene: SKScene, GADRewardBasedVideoAdDelegate {
 		self.backgroundColor = UIColor(displayP3Red: 0.2, green: 0.4, blue: 0.4, alpha: 1)
 	}
 }
-
-
-
